0b497c35f510af4c8c9564b23d76f01ce877cea4
wood.wu <kilokingw@gmail.com>
2023-05-24 13:08:25 +0800

    Updated pic

--- src/Section05.js
+++ src/Section05.js
@@ -1,14 +1,43 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from "swiper";
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/pagination";
 function Section05() {
+  const imgs =[
+    {image:"section05_p01.png"},
+    {image:"section05_p02.png"},
+  ]
   return (
     <div className='flex '>
-      <div className='w-full'>
+      <div className='w-full bg-[#ac282c]'>
         <img src={process.env.PUBLIC_URL+'/images/section05_text.png'} alt="" className='w-full' data-aos="fade" data-aos-duration="900"/>
       </div>
-      <div className='w-full bg-black'>
-        <img src={process.env.PUBLIC_URL+'/images/section05_p01.png'} alt="" className='w-full' data-aos="fade" data-aos-duration="900" data-aos-delay="500"/>
+
+      <div className='w-[53%] bg-black'>
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={1}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }} 
+          modules={[Pagination,Autoplay]}
+          autoplay={{delay:1800}}
+          centeredSlides={true}
+          className='w-full'
+        >
+          {
+            imgs.map((item,index)=>{
+              return(
+                <SwiperSlide><img src={process.env.PUBLIC_URL+'/images/section05/'+ item.image} alt="" className='w-full' /></SwiperSlide>
+              )
+            })
+          }
+        </Swiper>
       </div>
+      
     </div>
   )
 }
